ceb0ae53eab6c846b5bc112e1b99c9d657c480c2
larsbow <larsbow@stud.ntnu.no>
2014-03-07 23:35:42 +0100

    Smårusk

--- src/gui/Hovedside.java
+++ src/gui/Hovedside.java
@@ -1,10 +1,14 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Hovedside extends JFrame {
 	
 	public Hovedside() {
 		super("Avtalekalender");
+		setSize(600, 600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,19 @@
 package gui;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+
 	public static void Main(String[] args) {
-		Hovedside frame = new Hovedside();
+		new Main().setVisible(true);;
+	}
+	
+	private Main() {
+		super("Kalender");
+		setSize(600,600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(true);
 	}
 }

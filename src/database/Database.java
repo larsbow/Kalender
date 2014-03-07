949bfa4fd185b1af7c4c4d6d3fbb8812bcc4ddd6
larsbow <larsbow@stud.ntnu.no>
2014-03-07 16:15:59 +0100

    Database
    Lagd database-klasse som kjører spørringer og endringer mot databasen.

---
+++ src/database/Database.java
@@ -0,0 +1,56 @@
+package database;
+
+import java.sql.*;
+
+public class Database {
+	private static Connection conn = null;
+	private static String url = "jdbc:mysql://localhost:3306/";
+	private static String dbName = "kalender";
+	private static String driver = "com.mysql.jdbc.Driver";
+	private static String userName = "root";
+	private static String password = "passord";
+	
+	public Database() {
+		try {
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url+dbName,userName,password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+	
+	public void close() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public ResultSet readQuery(String sql) {
+		Statement s;
+		try {
+			s = conn.createStatement();
+			ResultSet rs;
+			rs = s.executeQuery(sql);
+			return rs;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void updateQuery(String sql) {
+		Statement s;
+		try {
+			s = conn.createStatement();
+			s.executeUpdate(sql);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Connection getConnection() {
+		return conn;
+	}
+}
+

0f72ef14303042eb9389e923d9850f77ba97be4e
larsbow <larsbow@stud.ntnu.no>
2014-03-13 13:30:48 +0100

    AvtaleLogic og InboxLogic

--- src/logikk/AvtaleLogic.java
+++ src/logikk/AvtaleLogic.java
@@ -1,6 +1,7 @@
 package logikk;
 
 import java.awt.Component;
+import java.sql.ResultSet;
 
 import javax.swing.JOptionPane;
 
@@ -11,14 +12,28 @@ public class AvtaleLogic {
 	Database db = new Database();
 
 	public boolean lagAvtale(String dato, String start, String slutt, String beskrivelse, Object romid, String sted, String ansatt) {
+		if (dato.length() != 8 || start.length() != 4){
+			return false;
+		}
 		try {
 			db.updateQuery("INSERT INTO avtale VALUES (null, '" + dato + "','" + start + "','" + slutt + "','" + beskrivelse + "'," + romid + ",'" + sted + "','" + ansatt + "')");
-			int varseltid = Integer.parseInt(start) -100;
+			int varseltid = Integer.parseInt(start) +1990;
 			String varselstring = Integer.toString(varseltid);
 			while (varselstring.length() < 4){
 				varselstring = "0" + varselstring;
 			}
 			db.updateQuery("INSERT INTO varsel VALUES (null, 'alarm', '"+varselstring+"', '"+dato+"')");
+			
+			ResultSet rs = db.readQuery("SELECT avtaleid FROM avtale ORDER BY avtaleid DESC");
+			rs.next();
+			int avtaleid = rs.getInt(1);
+			ResultSet rs2 = db.readQuery("SELECT varselid FROM varsel ORDER BY varselid DESC");
+			rs2.next();
+			int varselid = rs2.getInt(1);
+			System.out.println(varselid+" og "+avtaleid+" og "+ansatt);		
+			System.out.println("INSERT INTO haravtale VALUES ("+avtaleid+", '"+ansatt+"', "+varselid+")");
+
+			db.updateQuery("INSERT INTO haravtale VALUES ("+avtaleid+", '"+ansatt+"', "+varselid+")");
 		} catch (Exception e) {
 			return false;
 		}
--- src/logikk/InboxLogic.java
+++ src/logikk/InboxLogic.java
@@ -26,7 +26,10 @@ public class InboxLogic {
 		ResultSet rs = db.readQuery("SELECT beskjed, avtaleid, tidspunkt, dato, haravtale.brukernavn FROM ansatt, haravtale, varsel WHERE haravtale.brukernavn = '"+this.bruker+"'");
 		try {
 			while (rs.next()){
-				varsel.add("Endring i avtale" +rs.getString(2) + ": " + rs.getString(1) + " skjedde " + Integer.toString(rs.getInt(3))+" "+Integer.toString(rs.getInt(4)));
+				if (!(rs.getString(1).equals("alarm"))) {
+					varsel.add("Endring i avtale" +rs.getString(2) + ": " + rs.getString(1) + " skjedde " + Integer.toString(rs.getInt(3))+" "+Integer.toString(rs.getInt(4)));
+
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -38,11 +41,11 @@ public class InboxLogic {
 	}
 
 	public void findAlarm(){
-		ResultSet rs = db.readQuery("SELECT avtale.avtaleid, avtale.starttid, avtale.dato, sted, romid, haravtale.brukernavn, varsel.dato, varsel.tidspunkt FROM haravtale, varsel, avtale WHERE haravtale.brukernavn = '"+this.bruker+"' AND haravtale.avtaleid = avtale.avtaleid");
+		ResultSet rs = db.readQuery("SELECT avtale.avtaleid, avtale.starttid, avtale.dato, sted, romid, haravtale.brukernavn, varsel.dato, varsel.tidspunkt, varsel.beskjed FROM haravtale, varsel, avtale WHERE haravtale.brukernavn = '"+this.bruker+"' AND haravtale.avtaleid = avtale.avtaleid");
 		try {
 			int count = 0;
 			while (rs.next()){
-				if (hasBeen(rs.getString(8), rs.getString(7))){
+				if (hasBeen(rs.getString(8), rs.getString(7)) && rs.getString(9).equals("alarm")){
 					alarm.add("ALARM: AvtaleID "+ rs.getString(1) + " starter klokken "+Integer.toString(rs.getInt(2))+" ved (DDMMYY) "+Integer.toString(rs.getInt(3))+" ");
 					if (rs.getObject(5) == null){
 						alarm.set(count, alarm.get(count)+" i "+rs.getString(4)+"\n");
@@ -63,11 +66,11 @@ public class InboxLogic {
 		int minutt = Integer.parseInt(tidspunkt.substring(2, 4));
 		int dag = Integer.parseInt(dato.substring(0, 2));
 		int maaned = Integer.parseInt(dato.substring(2, 4));
-		int aar = Integer.parseInt(dato.substring(4, 6));
+		int aar = Integer.parseInt(dato.substring(4, 8));
 
-		if (aar < (tidno.getYear()-100)){
+		if (aar < (tidno.getYear()+1990)){
 			return true;
-		} else if (aar == tidno.getYear() -100){
+		} else if (aar == (tidno.getYear() + 1990)){
 			if (maaned < tidno.getMonth()) {
 				return true;
 			} else if (maaned == tidno.getMonth()) {
